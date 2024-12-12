f47f69e5f4264d368e3609c2c8159822d0a1ffc1
Emmanuel200816.github.io <qenemm06@ecolecatholique.ca>
2024-12-12 09:28:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-function joueUnC(event) {
-  let key = event.key;
-  if (key == "z") { 
-    let text = "You pressed the 'z' key!";
-    document.getElementById("divAffiche").innerHTML = text;
-  }
-}
-    
+
 	const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
