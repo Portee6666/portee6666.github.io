d893f5f8c0ca1aaa6de2cca2445ca7aceceae54e
Emmanuel200816.github.io <qenemm06@ecolecatholique.ca>
2024-12-12 09:34:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('keydown', e => {
 })
 
 function playNote(key) {
- let text = "You pressed the " + key.dataset.note + " key!";
+ let text = "You pressed the " + key + " key!";
     document.getElementById("divAffiche").innerHTML = text;
   const noteAudio = document.getElementById(key.dataset.note)
   noteAudio.currentTime = 0
