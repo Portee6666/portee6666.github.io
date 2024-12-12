9555ef6d372f842b5da8afd14995b3f074177f17
Emmanuel200816.github.io <qenemm06@ecolecatholique.ca>
2024-12-12 09:55:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 	const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
@@ -31,7 +30,4 @@ function playNote(key) {
     key.classList.remove('active')
   })
 }
-
-
-
-
+		
